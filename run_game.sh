dfefa9fa3a75e2a5ed82835054ec8e2014bd9fd8
Maciej Sanocki <msanock@gmail.com>
2023-02-06 09:37:50 +0100

    split run script into game and server

---
+++ run_game.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn exec:java -D "exec.mainClass=edu.paintOnline.game.App" &
+mvn exec:java -D "exec.mainClass=edu.paintOnline.game.App"
---
+++ run_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mvn exec:java -D "exec.mainClass=edu.paintOnline.server.Main"
